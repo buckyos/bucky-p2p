a8cd9cf70f7d61ed9ae59a76569508eb07e79f44
wugren <wugren@gmail.com>
2025-02-28 11:51:50 +0800

    remove unnecessary files

--- cyfs-p2p/src/pn/service/events.rs
+++
@@ -1,8 +0,0 @@
-use bucky_crypto::AesKey;
-use crate::error::BdtResult;
-use super::proxy::ProxyDeviceStub;
-
-#[async_trait::async_trait]
-pub trait ProxyServiceEvents: Send + Sync {
-    async fn pre_create_tunnel(&self, mix_key: &AesKey, device_pair: &(ProxyDeviceStub, ProxyDeviceStub)) -> BdtResult<()>;
-}
--- cyfs-p2p/src/pn/service/mod.rs
+++
@@ -1,9 +0,0 @@
-mod command;
-pub mod proxy;
-mod service;
-mod events;
-
-
-pub use service::{Service, Config};
-pub use proxy::ProxyDeviceStub;
-pub use events::ProxyServiceEvents;
